80528066a1a21edd6df1bd491057ab609dd3182c
chrismsimpson <chris@deepscroll.com>
2024-12-30 21:16:13 +1100

    style

--- src/05-perspective/05-perspective.cpp
+++ src/05-perspective/05-perspective.cpp
@@ -133,23 +133,23 @@ NS::Menu* MyAppDelegate::createMenuBar()
 
     NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
     NS::String* quitItemName = NS::String::string("Quit ", UTF8StringEncoding)->stringByAppendingString(appName);
-    SEL quitCb = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* sender) {
+    SEL quitCallback = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* sender) {
         auto app = NS::Application::sharedApplication();
         app->terminate(sender);
     });
 
-    NS::MenuItem* appQuitItem = appMenu->addItem(quitItemName, quitCb, NS::String::string("q", UTF8StringEncoding));
+    NS::MenuItem* appQuitItem = appMenu->addItem(quitItemName, quitCallback, NS::String::string("q", UTF8StringEncoding));
     appQuitItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
     appMenuItem->setSubmenu(appMenu);
 
     NS::MenuItem* windowMenuItem = NS::MenuItem::alloc()->init();
     NS::Menu* windowMenu = NS::Menu::alloc()->init(NS::String::string("Window", UTF8StringEncoding));
 
-    SEL closeWindowCb = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
+    SEL closeWindowCallback = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
         auto app = NS::Application::sharedApplication();
         app->windows()->object<NS::Window>(0)->close();
     });
-    NS::MenuItem* closeWindowItem = windowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCb, NS::String::string("w", UTF8StringEncoding));
+    NS::MenuItem* closeWindowItem = windowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCallback, NS::String::string("w", UTF8StringEncoding));
     closeWindowItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
 
     windowMenuItem->setSubmenu(windowMenu);
@@ -167,9 +167,9 @@ NS::Menu* MyAppDelegate::createMenuBar()
 
 void MyAppDelegate::applicationWillFinishLaunching(NS::Notification* notification)
 {
-    NS::Menu* pMenu = createMenuBar();
+    NS::Menu* menu = createMenuBar();
     NS::Application* app = reinterpret_cast<NS::Application*>(notification->object());
-    app->setMainMenu(pMenu);
+    app->setMainMenu(menu);
     app->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
 }
 
--- src/10-frame-debugging/10-frame-debugging.cpp
+++ src/10-frame-debugging/10-frame-debugging.cpp
@@ -157,59 +157,59 @@ NS::Menu* MyAppDelegate::createMenuBar()
 
     NS::String* appName = NS::RunningApplication::currentApplication()->localizedName();
     NS::String* quitItemName = NS::String::string("Quit ", UTF8StringEncoding)->stringByAppendingString(appName);
-    SEL quitCb = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* sender) {
+    SEL quitCallback = NS::MenuItem::registerActionCallback("appQuit", [](void*, SEL, const NS::Object* sender) {
         auto app = NS::Application::sharedApplication();
         app->terminate(sender);
     });
 
-    NS::MenuItem* appQuitItem = appMenu->addItem(quitItemName, quitCb, NS::String::string("q", UTF8StringEncoding));
+    NS::MenuItem* appQuitItem = appMenu->addItem(quitItemName, quitCallback, NS::String::string("q", UTF8StringEncoding));
     appQuitItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
     appMenuItem->setSubmenu(appMenu);
 
     NS::MenuItem* windowMenuItem = NS::MenuItem::alloc()->init();
     NS::Menu* windowMenu = NS::Menu::alloc()->init(NS::String::string("Window", UTF8StringEncoding));
 
-    SEL closeWindowCb = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
+    SEL closeWindowCallback = NS::MenuItem::registerActionCallback("windowClose", [](void*, SEL, const NS::Object*) {
         auto app = NS::Application::sharedApplication();
         app->windows()->object<NS::Window>(0)->close();
     });
-    NS::MenuItem* closeWindowItem = windowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCb, NS::String::string("w", UTF8StringEncoding));
+    NS::MenuItem* closeWindowItem = windowMenu->addItem(NS::String::string("Close Window", UTF8StringEncoding), closeWindowCallback, NS::String::string("w", UTF8StringEncoding));
     closeWindowItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
 
     windowMenuItem->setSubmenu(windowMenu);
 
     // Capture Menu UI
-    NS::MenuItem* pCaptureMenuItem = NS::MenuItem::alloc()->init();
-    NS::Menu* pCaptureMenu = NS::Menu::alloc()->init(NS::String::string("Capture", UTF8StringEncoding));
+    NS::MenuItem* captureMenuItem = NS::MenuItem::alloc()->init();
+    NS::Menu* captureMenu = NS::Menu::alloc()->init(NS::String::string("Capture", UTF8StringEncoding));
 
     SEL beginCaptureCb = NS::MenuItem::registerActionCallback("beginCapture", [](void*, SEL, const NS::Object*) {
         Renderer::beginCapture = true;
     });
 
-    NS::MenuItem* pBeginCaptureItem = pCaptureMenu->addItem(NS::String::string("Begin Capture", UTF8StringEncoding), beginCaptureCb, NS::String::string("c", UTF8StringEncoding));
+    NS::MenuItem* pBeginCaptureItem = captureMenu->addItem(NS::String::string("Begin Capture", UTF8StringEncoding), beginCaptureCb, NS::String::string("c", UTF8StringEncoding));
     pBeginCaptureItem->setKeyEquivalentModifierMask(NS::EventModifierFlagCommand);
 
-    pCaptureMenuItem->setSubmenu(pCaptureMenu);
+    captureMenuItem->setSubmenu(captureMenu);
 
     mainMenu->addItem(appMenuItem);
     mainMenu->addItem(windowMenuItem);
-    mainMenu->addItem(pCaptureMenuItem);
+    mainMenu->addItem(captureMenuItem);
 
     appMenuItem->release();
     windowMenuItem->release();
-    pCaptureMenuItem->release();
+    captureMenuItem->release();
     appMenu->release();
     windowMenu->release();
-    pCaptureMenu->release();
+    captureMenu->release();
 
     return mainMenu->autorelease();
 }
 
 void MyAppDelegate::applicationWillFinishLaunching(NS::Notification* notification)
 {
-    NS::Menu* pMenu = createMenuBar();
+    NS::Menu* menu = createMenuBar();
     NS::Application* app = reinterpret_cast<NS::Application*>(notification->object());
-    app->setMainMenu(pMenu);
+    app->setMainMenu(menu);
     app->setActivationPolicy(NS::ActivationPolicy::ActivationPolicyRegular);
 }
 
